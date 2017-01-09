a75cd5618bea5ae8a5b9af40139d72a49a22e432
Andrew Kane <acekane1@gmail.com>
2017-01-09 00:29:31 -0800

    Tested nameservers option

--- test/reversed_test.rb
+++ test/reversed_test.rb
@@ -5,6 +5,10 @@ class ReversedTest < Minitest::Test
     assert_equal "google-public-dns-b.google.com", Reversed.lookup("8.8.4.4")
   end
 
+  def test_nameservers
+    assert_equal "google-public-dns-b.google.com", Reversed.lookup("8.8.4.4", nameservers: ["8.8.8.8"])
+  end
+
   def test_nil
     assert_nil Reversed.lookup(nil)
   end
