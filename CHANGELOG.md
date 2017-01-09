5ac4461d3a435f31dca18be36ef2d2aefdd48d9a
Andrew Kane <acekane1@gmail.com>
2017-01-09 00:27:39 -0800

    Added timeout and nameservers option

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.2.1 [unreleased]
+
+- Added `timeout` and `nameservers` options
+
 ## 0.2.0
 
 - Removed unnecessary dependency
--- lib/reversed.rb
+++ lib/reversed.rb
@@ -2,12 +2,21 @@ require "reversed/version"
 require "net/dns"
 
 module Reversed
-  def self.lookup(ip)
+  def self.lookup(ip, timeout: 5, nameservers: nil)
     ip = ip.to_s
     unless ip.empty?
-      resolver = Net::DNS::Resolver.new(retry: 3).search(ip)
-      answer = resolver.answer.first || resolver.authority.first
-      answer.value.split(" ").first[0..-2] if answer
+      options = {
+        retry: 3,
+        udp_timeout: timeout
+      }
+      options[:nameservers] = nameservers if nameservers
+      begin
+        resolver = Net::DNS::Resolver.new(options).search(ip)
+        answer = resolver.answer.first || resolver.authority.first
+        answer.value.split(" ").first[0..-2] if answer
+      rescue Net::DNS::Resolver::NoResponseError
+        nil
+      end
     end
   end
 end
